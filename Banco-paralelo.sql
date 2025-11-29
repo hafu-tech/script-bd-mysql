fb822d82d935994f31f120f6670410e4dc3cdda0
GustavoVieck <gustavo.dores@sptech.school>
2025-11-29 12:52:06 -0300

    Atualização nas colunas e nas FK

--- Banco-paralelo.sql
+++ Banco-paralelo.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS hafutech;
 	
 USE hafutech;
 
-CREATE TABLE Escola(
+CREATE TABLE IF NOT EXISTS Escola(
     id INT PRIMARY KEY AUTO_INCREMENT,
     ano INT NOT NULL,
     id_municipio INT NOT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE Escola(
     regiao VARCHAR(45)
 );
 
-CREATE TABLE Status_log_sistema(
+CREATE TABLE IF NOT EXISTS Status_log_sistema(
 id INT PRIMARY KEY AUTO_INCREMENT,
 tipo VARCHAR(45),
 descricao_status TEXT
 );
 
-CREATE TABLE Log_sistema(
+CREATE TABLE IF NOT EXISTS Log_sistema(
 id INT PRIMARY KEY AUTO_INCREMENT,
 data_hora DATETIME,
 descricao_log TEXT,
@@ -44,7 +44,7 @@ REFERENCES Status_log_sistema(id)
 -- valor_inse DOUBLE
 -- );
 
-CREATE TABLE Empresa(
+CREATE TABLE IF NOT EXISTS Empresa(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome_fantasia VARCHAR(45),
 codigo_empresa VARCHAR(45),
@@ -53,13 +53,13 @@ senha VARCHAR(45),
 cnpj CHAR(14)
 );
 
-CREATE TABLE Status_log_empresa(
+CREATE TABLE IF NOT EXISTS Status_log_empresa(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(45),
 descricao TEXT
 );
 
-CREATE TABLE Log_historico_empresa(
+CREATE TABLE IF NOT EXISTS Log_historico_empresa(
 id INT AUTO_INCREMENT,
 data_hora DATETIME,
 descricao TEXT,
@@ -74,114 +74,71 @@ FOREIGN KEY (status_log_empresa_id)
 REFERENCES Status_log_empresa(id)
 );
 
-CREATE TABLE Parametrizacao_notificacao_usuario(
+CREATE TABLE IF NOT EXISTS Parametrizacao_notificacao_usuario(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome_parametro VARCHAR(50),
 parametro VARCHAR(50)
 );
 
-CREATE TABLE Status_notificacao_slack(
+CREATE TABLE IF NOT EXISTS Status_notificacao_slack(
 id INT PRIMARY KEY AUTO_INCREMENT,
 codigo_status_http CHAR(3),
 classificacao_status CHAR(3),
 descricao TEXT
 );
 
-CREATE TABLE Tipo_funcionario(
+CREATE TABLE IF NOT EXISTS Tipo_funcionario(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(45),
 descricao TEXT
 );
 
-CREATE TABLE Funcionario(
+CREATE TABLE IF NOT EXISTS Funcionario(
 id INT PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR(45),
 senha VARCHAR(45),
 nome_funcionario VARCHAR(45),
 fk_empresa INT,
 tipo_funcionario INT,
-status_slack_atividade_id INT,
-parametrizacao_notificacao_usuario_id INT,
-CONSTRAINT fk_funcionario_empresa
-FOREIGN KEY (fk_empresa)
-REFERENCES Empresa(id),
-CONSTRAINT fk_funcionario_tipo
-FOREIGN KEY (tipo_funcionario)
-REFERENCES Tipo_funcionario(id),
-CONSTRAINT fk_funcionario_param
-FOREIGN KEY (parametrizacao_notificacao_usuario_id)
-REFERENCES Parametrizacao_notificacao_usuario(id)
+CONSTRAINT fk_funcionario_empresa FOREIGN KEY (fk_empresa) REFERENCES Empresa(id),
+CONSTRAINT fk_funcionario_tipo FOREIGN KEY (tipo_funcionario) REFERENCES Tipo_funcionario(id)
 );
 
 
-CREATE TABLE Notificacao_slack(
-id INT PRIMARY KEY AUTO_INCREMENT,
-fk_empresa INT,
-mensagem TEXT,
-data_envio DATETIME,
-notificacao_codigo VARCHAR(45),
-funcionario_id INT,
-funcionario_tipo_funcionario INT,
-status_notificacao_slack_id INT,
-parametrizacao_notificacao_usuario_id INT,
-CONSTRAINT fk_notif_empresa
-FOREIGN KEY (fk_empresa)
-REFERENCES Empresa(id),
-CONSTRAINT fk_notif_funcionario
-FOREIGN KEY (funcionario_id)
-REFERENCES Funcionario(id),
-CONSTRAINT fk_notif_tipo_funcionario
-FOREIGN KEY (funcionario_tipo_funcionario)
-REFERENCES Tipo_funcionario(id),
-CONSTRAINT fk_notif_status
-FOREIGN KEY (status_notificacao_slack_id)
-REFERENCES Status_notificacao_slack(id),
-CONSTRAINT fk_notif_param
-FOREIGN KEY (parametrizacao_notificacao_usuario_id)
-REFERENCES Parametrizacao_notificacao_usuario(id)
+
+CREATE TABLE IF NOT EXISTS analise (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(45),
+    descricao VARCHAR(250),
+    fk_empresa INT,
+    funcionario_id INT,
+    funcionario_tipo_funcionario INT,
+    CONSTRAINT fk_analise_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id),
+    CONSTRAINT fk_analise_func FOREIGN KEY (funcionario_id) REFERENCES funcionario(id),
+    CONSTRAINT fk_analise_tipo_func FOREIGN KEY (funcionario_tipo_funcionario) REFERENCES tipo_funcionario(id)
 );
 
-CREATE TABLE Analise(
-id INT PRIMARY KEY AUTO_INCREMENT,
-titulo VARCHAR(45),
-descricao VARCHAR(250),
-fk_empresa INT,
-funcionario_id INT,
-funcionario_tipo_funcionario INT,
-CONSTRAINT fk_analise_empresa
-FOREIGN KEY (fk_empresa)
-REFERENCES Empresa(id),
-CONSTRAINT fk_analise_func
-FOREIGN KEY (funcionario_id)
-REFERENCES Funcionario(id),
-CONSTRAINT fk_analise_tipo_func
-FOREIGN KEY (funcionario_tipo_funcionario)
-REFERENCES Tipo_funcionario(id)
-);
 
-CREATE TABLE Status_slack_atividade(
-id INT PRIMARY KEY AUTO_INCREMENT,
-estado INT,
-notificacao_slack_id INT,
-notificacao_slack_fk_empresa INT,
-notificacao_slack_funcionario_id INT,
-notificacao_slack_funcionario_tipo_funcionario INT,
-empresa_id INT,
-CONSTRAINT fk_st_atividade_notif
-FOREIGN KEY (notificacao_slack_id)
-REFERENCES Notificacao_slack(id),
-CONSTRAINT fk_st_atividade_empresa
-FOREIGN KEY (empresa_id)
-REFERENCES Empresa(id),
-CONSTRAINT fk_st_atividade_func
-FOREIGN KEY (notificacao_slack_funcionario_id)
-REFERENCES Funcionario(id),
-CONSTRAINT fk_st_atividade_tipo
-FOREIGN KEY (notificacao_slack_funcionario_tipo_funcionario)
-REFERENCES Tipo_funcionario(id)
+CREATE TABLE IF NOT EXISTS status_slack_atividade (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    estado ENUM('ATIVO','INATIVO'),
+    empresa_id INT,
+    CONSTRAINT fk_status_atividade_empresa FOREIGN KEY (empresa_id) REFERENCES empresa(id)
 );
-
-ALTER TABLE Funcionario
-ADD CONSTRAINT fk_funcionario_status_slack
-    FOREIGN KEY (status_slack_atividade_id)
-    REFERENCES Status_slack_atividade(id);
+    
+   
+CREATE TABLE IF NOT EXISTS notificacao_slack (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_empresa INT,
+    mensagem TEXT,
+    data_envio DATETIME,
+    parametrizacao_notificacao_usuario_id INT,
+    status_slack_atividade_id INT,
+    status_notificacao_slack_id INT,
+    CONSTRAINT fk_notif_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id),
+    CONSTRAINT fk_notif_param FOREIGN KEY (parametrizacao_notificacao_usuario_id) REFERENCES parametrizacao_notificacao_usuario(id),
+    CONSTRAINT fk_notif_status_atividade FOREIGN KEY (status_slack_atividade_id) REFERENCES status_slack_atividade(id),
+    CONSTRAINT fk_notif_status_slack FOREIGN KEY (status_notificacao_slack_id) REFERENCES status_notificacao_slack(id)
+);
+    
+		
